a57bcf7d5072a308387d96d91de5acb49adf1b15
to101380 <to101380@gmail.com>
2022-04-22 10:38:44 +0800

    NFT test

--- remixd/subscribe.sol
+++ remixd/subscribe.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.7;
 
 // CAUTION
 // This version of SafeMath should only be used with Solidity 0.8 or later,
--- remixd/treasury.sol
+++ remixd/treasury.sol
@@ -231,7 +231,7 @@ interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address to, uint256 amount) external returns (bool); 
-    function burnFrom(address account, uint256 amount) external;
+    function burnFrom(address account, uint256 amount) external;     
 }
 
 
