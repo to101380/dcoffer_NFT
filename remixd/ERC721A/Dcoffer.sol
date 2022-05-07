55e6ba46da675ec1d7953f607f6d0828f61c284b
to101380 <to101380@gmail.com>
2022-05-08 01:25:21 +0800

    ERC721A

---
+++ remixd/ERC721A/Dcoffer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC721A.sol";
+
+contract Dcoffer is ERC721A {
+  constructor() ERC721A("Dcoffer", "Dcoffer") {}
+
+  function mint(uint256 quantity) external payable {
+    // _safeMint's second argument now takes in a quantity, not a tokenId.
+    _safeMint(msg.sender, quantity);
+  }
+}
