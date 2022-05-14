9edb7fb00be285620f5f8be1e5f5f9d7248c5209
to101380 <to101380@gmail.com>
2022-05-14 20:47:03 +0800

    mineStatus

--- remixd/Dcoffer/ERC20/DCOFF.sol
+++ remixd/Dcoffer/ERC20/DCOFF.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DCOFF is ERC20 {
-    constructor() ERC20("DCOFF", "DCF") {
+    constructor() ERC20("Dcoff", "DCF") {
         _mint(msg.sender, 100000000 * 10 ** decimals());
     }
 }
--- remixd/Dcoffer/treasury.sol
+++ remixd/Dcoffer/treasury.sol
@@ -7,8 +7,8 @@ import './IDCF/IDCF.sol';
 
 contract treasury  {
   using SafeMath for uint;  
-  IDCF mine = IDCF(0xbc1ADfF2EAC8dbA22d382DFADDf7e6B73aBC2334);
-  IERC20 dcoff =  IERC20(0xbc1ADfF2EAC8dbA22d382DFADDf7e6B73aBC2334);
+  IDCF mine = IDCF(0x6a544367FCa7064B6ADA5dc87F7E5058A9bB7Af9);
+  IERC20 dcoff =  IERC20(0xbE0C856e2981b9f03d7613566DFC6679eEe4708A);
 
   uint256 DcoffperBlock;  
   mapping(address => uint256 )private ScoreWithdrawn;
@@ -18,15 +18,27 @@ contract treasury  {
   }
 
 
-  function MineDcoff()external{
-      uint256 Score = mine.BlockScore(msg.sender);
-      uint256 afterScore = Score.sub(ScoreWithdrawn[msg.sender]);
-      uint256 GetDCF = afterScore.mul(DcoffperBlock);
+  function MineDcoff()external{      
+      uint256 GetDCF = mineStatus(msg.sender);
       dcoff.transfer(msg.sender,GetDCF);
+
+      uint256 afterScore = Score(msg.sender);
       ScoreWithdrawn[msg.sender] = ScoreWithdrawn[msg.sender].add(afterScore);
   }  
 
+  function Score(address user)internal view returns(uint256){
+      uint256 _Score = mine.BlockScore(user);
+      uint256 afterScore = _Score.sub(ScoreWithdrawn[user]);
+      return afterScore;
+  } 
 
+  function mineStatus(address user)public view returns(uint256){
+      uint256 afterScore = Score(user);
+      uint256 GetDCF = afterScore.mul(DcoffperBlock);
+      return GetDCF;
+  }
+
+   
   
 
 }
